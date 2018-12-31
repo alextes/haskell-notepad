0f9bedce28e5f2f75c9a78e4e5a393e6e799f5b6
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2018-12-31 13:15:22 +0100

    Add PoemWriter

---
+++ PoemWriter.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+
+module PoemWriter where
+
+import           Data.Text
+import Data.Text (pack)
+import           NeatInterpolation
+
+helpText :: Text
+helpText =
+  [text|
+    help        see this menu, obviously
+    say         say some text placed in a ascii banner
+    poem        say a little poem for your muse
+    exit        say bye!
+  |]
+
+decorate :: Text -> Text
+decorate message =
+  [text|
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    $message
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  |]
+
+writePoem :: Text -> Text
+writePoem muse =
+  [text|
+    Dear $muse,
+
+    Roses are not blue
+    Violets are not red
+    Whatever you do
+    It trumps being dead
+  |]
+
+exec :: [Text] -> Text
+exec ["poem", muse] = writePoem muse
+exec ["say", message] = decorate message
+exec ["exit"] = "Bye cruel world!"
+exec ["help"] = helpText
+exec x =
+  [text|"'${input}' was an example of an incorrect command"|]
+  where
+    input = Data.Text.unwords x
+
+main = do
+  input <- getLine
+  let textInput = pack input
+  let textOutput = exec $ Data.Text.words textInput
+  putStrLn $ unpack textOutput
